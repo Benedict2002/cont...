f41b2ebe1d0858eb62c3dea1a706317a16ae6513
Benedict juma <94790520+Benedict2002@users.noreply.github.com>
2024-02-03 23:35:37 +0300

    Add files via upload

---
+++ testData/src/Dt/data.java
@@ -0,0 +1,21 @@
+package Dt;
+
+
+
+
+public class data {
+public static void main(String[]args){
+
+
+
+    int a=-5+8*6;
+    int b=(55+9)%9;
+    int c=20+-3*5/8;
+    int d=5+15/3*2-8%3;
+
+    System.out.println("value of a : "+a);
+    System.out.println("value of b : "+b);
+    System.out.println("value of c : "+c);
+    System.out.println("value of d : "+d);
+                                        }
+}
